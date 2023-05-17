0fc0df3ccfad44155329ffae2fdc500905cc1833
Marten Haitjema <martenh99@gmail.com>
2023-05-17 16:21:28 +0200

    Some styling

--- AnalyzeData.py
+++ AnalyzeData.py
@@ -9,51 +9,60 @@ from data import bike_without_rider
 from model import SteerControlModel
 
 def main():
-    path = "finalized_logs"
+    dir = os.path.join("finalized_logs", "bas-on-gain-6")
+    prefix = "12-May-2023-10-11-04-bas-on-gain-6-speed-"
     number_of_files = 3
     show_plots = False
 
-    prefix = "12-May-2023-10-11-04-bas-on-gain-6-speed-"
     velocities = [6, 8, 10, 12, 14, 16, 18]
-    eigenvalues_real = []
-    eigenvalues_img = []
+    avg_eigenvalues_real = []
+    avg_eigenvalues_img = []
 
     for j, vel in enumerate(velocities): 
-        eig_real = []
-        eig_img = []
+        eigenvalues_real = []
+        eigenvalues_real = []
         for i in np.linspace(1, number_of_files, number_of_files, dtype=int):
-            path = os.path.join("finalized_logs", "bas-on-gain-6", prefix + str(vel))
+            path = os.path.join(dir, prefix + str(vel))
             df_gyro = pd.read_csv(path + "-gyro-" + str(i) + ".csv")
 
             popt, pcov = spo.curve_fit(kooijman_func,
                                 df_gyro.loc[:,"time"],
                                 df_gyro.loc[:,"gyro_x"],
                                 p0=(0.03, -1.0, 1.0, 7.0, -0.7))
-            eig_real.append(popt[1])
-            eig_img.append(popt[3])
+            eigenvalues_real.append(popt[1])
+            eigenvalues_real.append(popt[3])
 
             if show_plots:
                 print(popt)
                 fig, ax = plt.subplots(1,1)
                 ax.plot(df_gyro.loc[:,"time"], df_gyro.loc[:,"gyro_x"], '.')
-                ax.plot(df_gyro.loc[:,"time"], kooijman_func(df_gyro.loc[:,"time"], popt[0], popt[1], popt[2], popt[3], popt[4]))
+                ax.plot(
+                    df_gyro.loc[:,"time"],
+                    kooijman_func(
+                        df_gyro.loc[:,"time"], popt[0], popt[1], popt[2], popt[3], popt[4]
+                    )
+                )
                 ax.set_xlabel("Time since perturbation [s]")
                 ax.set_ylabel("Roll rate [rad/s]")
                 fig.show()
                 fig.waitforbuttonpress()
 
 
-        eigenvalues_real.append(sum(eig_real)/np.size(eig_real))
-        eigenvalues_img.append(sum(eig_img)/np.size(eig_img))
+        avg_eigenvalues_real.append(sum(eigenvalues_real)/np.size(eigenvalues_real))
+        avg_eigenvalues_img.append(sum(eigenvalues_real)/np.size(eigenvalues_real))
 
     plot_theoretical_eigenvalues()
 
-    print(f"Real: {eigenvalues_real}")
-    print(f"Imaginary: {eigenvalues_img}")
+    print(f"Real: {avg_eigenvalues_real}")
+    print(f"Imaginary: {avg_eigenvalues_img}")
 
     velocities_ms = [vel / 3.6 for vel in velocities]
-    plt.plot(velocities_ms, eigenvalues_real, '.', label="Measured real eigenvalues")
-    plt.plot(velocities_ms, eigenvalues_img, '.', label='Measured imaginary eigenvalues')
+    plt.plot(
+        velocities_ms, avg_eigenvalues_real, '.', label="Measured real eigenvalues"
+    )
+    plt.plot(
+        velocities_ms, avg_eigenvalues_img, '.', label='Measured imaginary eigenvalues'
+    )
     plt.legend()
     plt.waitforbuttonpress()
 
